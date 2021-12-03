552b24b48c90debb47e8298b4d0ffbe0331f4998
nKaur1 <navneet.kaur@morningstar.com>
2021-12-03 19:19:55 +0530

    Added Console App and Iterate on Name list

---
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.IterateName(0);
+
+        }
+    }
+}
---
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+  public  class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+                    {
+                    "Jimmy",
+                    "Jeffrey",
+                    "John",
+                    };
+
+
+        public static void IterateName(int i)
+        {
+            if (i >= NamesList.Count)
+            {
+                return;
+            }
+            Console.WriteLine(NamesList[i]);
+            IterateName(i + 1);
+        }
+    }
+}
